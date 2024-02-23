d4fcf65a16c00385aac6e1ed50099412b82c7fc0
armstrong <armstrong@binada.lan>
2024-02-23 07:31:15 +0100

    notification event handler

--- notifications/src/Application/Event/EventHandler.php
+++ notifications/src/Application/Event/EventHandler.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Application\Event;
 
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-use App\Domain\Event\UserRegisteredEvent;
+use App\Domain\User\Events\UserRegisteredEvent;
 use Psr\Log\LoggerInterface;
 
 class EventHandler implements MessageHandlerInterface
